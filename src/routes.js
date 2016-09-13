a07d1e44b70db4e36f6086db6a764c8c9ac5fc2b
Marc Garreau <marcdgarreau@gmail.com>
2016-09-13 11:47:08 -0600

    Adds an IndexRoute

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { IndexRoute, Route } from 'react-router';
 import App from './containers/App';
 import About from './components/About';
 import ListPage from './containers/ListPage';
 
 export default (
   <Route path='/' component={App}>
+    <IndexRoute component={About} />
     <Route path='/about' component={About} />
     <Route path='/list' component={ListPage} />
   </Route>
