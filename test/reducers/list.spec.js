8531293e112b54bd77b692a0989e6ce3eb61f45a
Marc Garreau <marcdgarreau@gmail.com>
2016-09-14 14:50:51 -0600

    Tests the list reducer

---
+++ test/reducers/list.spec.js
@@ -0,0 +1,43 @@
+import expect from 'expect';
+import reducer from '../../src/reducers';
+import * as types from '../../src/actions/ActionTypes';
+
+describe('list reducer', () => {
+  it('should return the initial state', () => {
+    const initialState = undefined;
+    const action = {};
+    const nextState = reducer(initialState, action);
+
+    expect(nextState.list).toEqual(
+      {
+        items: ['example item']
+      }
+    );
+  });
+
+  it('should handle ADD_ITEM', () => {
+    const initialState = {
+      list: {
+        items: [
+          'example item',
+          'example two'
+        ]
+      }
+    };
+    const action = {
+      type: types.ADD_ITEM,
+      item: 'example three'
+    };
+    const nextState = reducer(initialState, action);
+
+    expect(nextState.list).toEqual(
+      {
+        items: [
+          'example item',
+          'example two',
+          'example three'
+        ]
+      }
+    );
+  });
+});
