ffb363bfea5d58b989cb774a865c79764295c426
Alexander Lee <leeajl006@gmail.com>
2016-12-08 01:46:31 -0500

    Remove mount dev tools

--- src/containers/Root.dev.js
+++ src/containers/Root.dev.js
@@ -2,8 +2,6 @@ import React, { Component, PropTypes } from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router';
 import routes from '../routes';
-import DevTools from './DevTools';
-
 
 class Root extends Component {
   render() {
@@ -13,7 +11,6 @@ class Root extends Component {
         <Provider store={store}>
           <div>
             <Router history={history} routes={routes} />
-            <DevTools />
           </div>
         </Provider>
       </div>
