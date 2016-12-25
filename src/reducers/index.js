fc2e99ee5a9a1f530f15562ea1cd7b274d29d95e
Alex Lee <alex@uniteus.com>
2016-12-25 13:10:39 -0800

    redux form and lodash

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
+import { reducer as formReducer } from 'redux-form';
 import flashMessages from './flashMessages';
 import list from './list';
 
 const rootReducer = combineReducers({
   flashMessages,
   list,
-  routing
+  routing,
+  form: formReducer,
 });
 
 export default rootReducer;
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { IndexRoute, Route } from 'react-router';
 import App from './containers/App';
 import About from './components/About';
 import ListPage from './containers/ListPage';
+import ReduxFormSample from './components/ReduxFormSample';
 
 export default (
   <Route path='/' component={App}>
     <IndexRoute component={About} />
     <Route path='/about' component={About} />
     <Route path='/list' component={ListPage} />
+    <Route path='/form' component={ReduxFormSample} />
   </Route>
 );
