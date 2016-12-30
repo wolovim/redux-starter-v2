101ee500563bb3307edd4f3ceb59bf1a0835767e
Alex Lee <alex@uniteus.com>
2016-12-30 00:30:25 -0800

    Redux Immutable setup

--- src/index.js
+++ src/index.js
@@ -6,9 +6,16 @@ import { browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import './axiosConfig';
 import './assets/css/index.scss';
+import Immutable from 'immutable';
 
-const store = configureStore();
-const history = syncHistoryWithStore(browserHistory, store);
+const initialState = Immutable.Map();
+const store = configureStore(initialState);
+
+const history = syncHistoryWithStore(browserHistory, store, {
+  selectLocationState (state) {
+      return state.get('routing').toObject();
+  }
+});
 
 render(
   <Root store={store} history={history} />,
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,14 @@
-import { combineReducers } from 'redux';
-import { routerReducer as routing } from 'react-router-redux';
+import { combineReducers } from 'redux-immutable';
+import routing from 'reducers/routingReducer.js';
 import { reducer as formReducer } from 'redux-form';
 import flashMessages from './flashMessages';
 import list from './list';
 import loginReducer from 'Login/reducers/LoginReducer.jsx';
 
 const rootReducer = combineReducers({
+  routing,
   flashMessages,
   list,
-  routing,
   form: formReducer,
   loginReducer,
 });
